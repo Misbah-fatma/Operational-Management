0a2ed55a43caaa71a1db6642fe7e3fb2891d5520
Misbah Fatma <misbah7370@gmail.com>
2026-07-11 20:48:19 +0530

    Fixed cors error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,12 +13,7 @@ import logger from './config/logger';
 const app = express();
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
-app.use(
-  cors({
-    origin: env.clientUrl,
-    credentials: true,
-  }),
-);
+app.use( cors());
 app.use(morgan('combined', { stream: { write: (msg) => logger.info(msg.trim()) } }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
